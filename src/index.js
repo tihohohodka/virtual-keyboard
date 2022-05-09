8ae6c8b3e14264159129e9a0459a9e39e22917ce
tihohohodka <kikboo1999@gmail.com>
2022-05-09 18:45:08 +0300

    feat:add more letters and CapsLock

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ if (event.code==='CapsLock'){
 
 }, false);
 document.addEventListener("keyup",function(event) {
-    console.log(event.code);    
+    console.log(event.key);    
 if (event.code==='ShiftLeft'||event.code==='ShiftRight'){
 isShift=false;
 
@@ -65,8 +65,8 @@ this.Key=value;
 this.KeyShift=shiftValue;
 this.rusKey=rusvalue;
 this.rusShift=rusShift;
-this.width= 25;
-this.height= 25;
+this.width= 32;
+this.height= 32;
 }
 
 create(){
@@ -101,9 +101,9 @@ create(){
                 }
 
         }
-    
-        if ((event.code===`Key${thisButton.Key.toUpperCase()}`)||(event.code===`Digit${thisButton.Key}`)){
-            thisButton.classList.toggle('pressed');
+        
+        if ((event.code===`Key${thisButton.Key.toUpperCase()}`)||(event.code===`Digit${thisButton.Key}`)||(event.key==thisButton.Key)||(event.key==thisButton.KeyShift)||(event.key==thisButton.rusShift)||(event.key==thisButton.rusKey)){
+            thisButton.classList.add('pressed');
             if (language==='rus'){if (isShift){
                 let x = textArea.value;
                 textArea.setSelectionRange(textArea.selectionStart, textArea.selectionStart);
@@ -126,7 +126,7 @@ create(){
     })
         
     thisButton.addEventListener('mousedown',function(){
-        thisButton.classList.toggle('pressed');
+        thisButton.classList.add('pressed');
         if (language==='rus'){if (isShift){
             let x = textArea.value;
             textArea.setSelectionRange(textArea.selectionStart, textArea.selectionStart);
@@ -147,17 +147,98 @@ create(){
 
     })
     thisButton.addEventListener('mouseup',function(){
-        thisButton.classList.toggle('pressed');})
+        thisButton.classList.remove('pressed');})
         document.addEventListener("keyup",function(event) {
-            if ((event.code===`Key${thisButton.Key.toUpperCase()}`)||(event.code===`Digit${thisButton.Key}`)){
-                thisButton.classList.toggle('pressed');
+            if ((event.code===`Key${thisButton.Key.toUpperCase()}`)||(event.code===`Digit${thisButton.Key}`)||(event.key==thisButton.Key)||(event.key==thisButton.KeyShift)||(event.key==thisButton.rusShift)||(event.key==thisButton.rusKey)){
+                thisButton.classList.remove('pressed');
         }})
     
            
 
 }
 }
+class funcButton{
 
+
+    constructor(name){
+    this.name=name
+    this.width=60;
+    this.height= 32;
+    }
+    create(){
+        
+            let thisButton=document.createElement("div");
+            let text = document.createElement('p');
+           
+            text.innerHTML=this.name;   
+            thisButton.style.width=this.width+'px';
+            thisButton.style.height=this.height+'px';
+            thisButton.style.display='flex';
+            thisButton.style.alignItems='center';
+            thisButton.style.justifyContent='center';
+            thisButton.classList.add('funcButtonMy');
+            thisButton.appendChild(text);
+            keyboard.appendChild(thisButton);
+            thisButton.name=this.name;
+            thisButton.addEventListener('mousedown',function(event){
+                thisButton.classList.add('pressed');
+
+            });
+            thisButton.addEventListener('mouseup',function(){
+                thisButton.classList.remove('pressed');
+            })
+            
+                if (thisButton.name==='Backspace'){document.addEventListener("keydown",function(event) {
+                    if (event.key==='Backspace'){
+                    let  x =textArea.value;
+                     textArea.value = x.slice(0, textArea.value.length-1)}})}
+                
+                if (thisButton.name==='Tab'){document.addEventListener("keydown",function(event) {
+                    if (event.key==='Tab'){  textArea.value +='\t' }})}
+                
+                if (thisButton.name==='Delete'){document.addEventListener("keydown",function(event) {
+                    if (event.key==='Delete'){
+                    let x = textArea.value;
+                    textArea.value = x.slice(0, textArea.selectionStart) + x.slice(textArea.selectionStart+1);}})}     
+                    
+                   
+                
+
+                    document.addEventListener("keydown",function(event) {
+                        if (event.key.name===thisButton.name){
+                    thisButton.classList.add('pressed');}
+                }
+                )
+                
+            document.addEventListener("keyup",function(event) {
+                
+                if ((event.key===thisButton.name)){
+                    thisButton.classList.remove('pressed');
+            }})
+            if (this.name==='Backspace'){
+                thisButton.addEventListener('click',e=>{ 
+                   let  x =textArea.value;
+                    textArea.value = x.slice(0, textArea.value.length-1)
+                });
+            }   
+                if (this.name==='Tab'){
+                    thisButton.addEventListener('click',e=>{ 
+                    textArea.value +='\t' }
+                )}
+                if (this.name==='Delete'){
+                    thisButton.addEventListener('click',e=>{ 
+                        let x = textArea.value;
+                        textArea.value = x.slice(0, textArea.selectionStart) + x.slice(textArea.selectionStart+1); }
+                )}
+                if (this.name==='CapsLock'){
+                    thisButton.addEventListener('click',e=>{ 
+                        isShift=!isShift; }
+                )}
+  
+  
+            
+    }
+}    
 window.onload = function()
 {
     const body = document.body;
@@ -188,12 +269,66 @@ if (!language){
 
 }
 
-
-
-let qbut = new button('q','й','Q','Й');
-qbut.create();
+let yobut = new button('`','ё','~','Ё');
+yobut.create();
 let onebut = new button('1','1','!','!');
 onebut.create();
+let twobut = new button('2','2','@','"');
+twobut.create();
+let threebut = new button('3','3','#','№');
+threebut.create();
+let fourbut = new button('4','4','$',';');
+fourbut.create();
+let fivebut = new button('5','5','%','%');
+fivebut.create();
+let sixbut = new button('6','6','^',':');
+sixbut.create();
+let sevbut = new button('7','7','&','&');
+sevbut.create();
+let eibut = new button('8','8','*','*');
+eibut.create();
+let ninbut = new button('9','9','(','(');
+ninbut.create();
+let nullbut = new button('0','0',')',')');
+nullbut.create();
+let minbut = new button('-','-','_','_');
+minbut.create();
+let eqbut = new button('=','=','+','+');
+eqbut.create();
+let slbut = new button('\\','\\','|','/');
+slbut.create();
+let bckbut = new funcButton('Backspace');
+bckbut.create();
+let tabbut = new funcButton('Tab');
+tabbut.create();
+let qbut = new button('q','й','Q','Й');
+qbut.create();
+let wbut = new button('w','ц','W','Ц');
+wbut.create();
+let ebut = new button('e','у','E','У');
+ebut.create();
+let rbut = new button('r','к','R','К');
+rbut.create();
+let tbut = new button('t','е','T','Е');
+tbut.create();
+let ybut = new button('y','н','Y','Н');
+ybut.create();
+let ubut = new button('u','г','U','Г');
+ubut.create();
+let ibut = new button('i','ш','I','Ш');
+ibut.create();
+let obut = new button('o','щ','O','Щ');
+obut.create();
+let pbut = new button('p','з','P','З');
+pbut.create();
+let hebut = new button('[','х','{','Х');
+hebut.create();
+let tzbut = new button(']','ъ','}','Ъ');
+tzbut.create();
+let delbut = new funcButton('Delete');
+delbut.create();
+let cpsbut = new funcButton('CapsLock');
+cpsbut.create();
 
 }
 function readCookie(name) {
